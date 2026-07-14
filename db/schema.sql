0b312ad72fe1e2b87da67dfdc7a3ffb2147400ec
Nadzeya Zianiuk <nadzeya@nadzeyas-air.mynetworksettings.com>
2026-07-13 20:02:51 -0400

    schemas created

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
 -- TODO
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE orders(
+    id serial PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products(
+    id serial PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL NOT NULL
+);
+
+CREATE TABLE orders_products(
+    order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    products_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL
+);
